bd524b8b63585f7f1cd3554ed0295fefe2bc51e1
Jesper De Temmerman <jesper@toron.be>
2009-08-24 20:55:15 +0200

    Added an assembly based statementresolver provider

---
+++ src/main/JDT.Calidus.Parsers/Statements/Resolvers/AssemblyBasedStatementResolverProvider.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using System.IO;
+
+namespace JDT.Calidus.Parsers.Statements.Resolvers
+{
+    /// <summary>
+    /// This class provides an assembly-based statement resolver provider
+    /// </summary>
+    public class AssemblyBasedStatementResolverProvider : IStatementResolverProvider
+    {
+        private static IEnumerable<Assembly> GetCurrentDirectoryAssemblyList()
+        {
+            IList<Assembly> assemblyList = new List<Assembly>();
+            foreach(String anAssembly in Directory.GetFiles(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "*.dll"))
+            {
+                assemblyList.Add(Assembly.LoadFile(anAssembly));
+            }
+            return assemblyList;
+        }
+
+        private IEnumerable<Assembly> _assemblies;
+
+        /// <summary>
+        /// Create a new instance of this class
+        /// </summary>
+        public AssemblyBasedStatementResolverProvider()
+            : this(GetCurrentDirectoryAssemblyList())
+        {
+        }
+
+        /// <summary>
+        /// Create a new instance of this class
+        /// </summary>
+        /// <param name="assemblyToParse">The assembly to parse</param>
+        public AssemblyBasedStatementResolverProvider(Assembly assemblyToParse)
+            : this(new Assembly[] { assemblyToParse})
+        {
+        }
+
+        /// <summary>
+        /// Create a new instance of this class
+        /// </summary>
+        /// <param name="assembliesToParse"></param>
+        public AssemblyBasedStatementResolverProvider(IEnumerable<Assembly> assembliesToParse)
+        {
+            _assemblies = assembliesToParse;
+        }
+
+        /// <summary>
+        /// Loads the resolvers from the provider
+        /// </summary>
+        /// <returns>The resolvers</returns>
+        public IEnumerable<IStatementResolver> GetResolvers()
+        {
+            IList<IStatementResolver> resolvers = new List<IStatementResolver>();
+
+            //loads all types in the assembly and checks that they are
+            //implementations of the IStatementResolver
+            foreach (Assembly anAssembly in _assemblies)
+            {
+                foreach (Type aType in anAssembly.GetTypes())
+                {
+                    //make sure to ignore the interface itself
+                    if (typeof(IStatementResolver).IsAssignableFrom(aType)
+                        && aType.IsInterface == false)
+                    {
+                        resolvers.Add((IStatementResolver)Activator.CreateInstance(aType));
+                    }
+                }
+            }
+
+            return resolvers;
+        }
+    }
+}
---
+++ src/test/JDT.Calidus.ParsersTest/Statements/Resolvers/AssemblyBasedStatementResolverProviderTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Reflection;
+using JDT.Calidus.Parsers.Statements;
+using JDT.Calidus.Tokens;
+using JDT.Calidus.Statements;
+using JDT.Calidus.Parsers.Statements.Resolvers;
+
+namespace JDT.Calidus.ParsersTest.Statements.Resolvers
+{
+    public class TestStatementResolver : IStatementResolver
+    {
+        public StatementBase Resolve(IList<TokenBase> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CanResolve(IList<TokenBase> tokenList)
+        {
+            throw new NotImplementedException();
+        }
+
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            //       
+            // See the full list of guidelines at
+            //   http://go.microsoft.com/fwlink/?LinkID=85237  
+            // and also the guidance for operator== at
+            //   http://go.microsoft.com/fwlink/?LinkId=85238
+            //
+
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            TestStatementResolver theResolver = (TestStatementResolver)obj;
+            return true;
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            int hash = 32;
+            return hash;
+        }
+    }
+
+    [TestFixture]
+    public class AssemblyBasedStatementResolverProviderTest
+    {
+        private AssemblyBasedStatementResolverProvider _resolverProvider;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Assembly toCheck = this.GetType().Assembly;
+            _resolverProvider = new AssemblyBasedStatementResolverProvider(toCheck);
+        }
+
+        [Test]
+        public void TestStatementResolverInstancesShouldBeEqual()
+        {
+            TestStatementResolver alpha = new TestStatementResolver();
+            TestStatementResolver bravo = new TestStatementResolver();
+
+            Assert.AreEqual(alpha, bravo);
+        }
+
+        [Test]
+        public void AssemblyBasedStatementResolverProviderShouldReturnResolversInAssemblyList()
+        {
+            IList<IStatementResolver> expected = new List<IStatementResolver>();
+            expected.Add(new TestStatementResolver());
+
+            CollectionAssert.AreEquivalent(expected, _resolverProvider.GetResolvers());
+        }
+    }
+}
