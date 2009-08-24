aa1ce6ddb6e6955ffc16ef42b6a2b732078593b4
Jesper De Temmerman <jdt@users.noreply.github.com>
2009-08-24 19:36:04 +0200

    Added a test for the default statement resolver to check for an empty result set

---
+++ src/test/JDT.Calidus.ParsersTest/Statements/Resolvers/DefaultStatementResolverProviderTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using JDT.Calidus.Parsers.Statements.Resolvers;
+
+namespace JDT.Calidus.ParsersTest.Statements.Resolvers
+{
+    [TestFixture]
+    public class DefaultStatementResolverProviderTest
+    {
+        private DefaultStatementResolverProvider _statementFactory;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _statementFactory = new DefaultStatementResolverProvider();
+        }
+
+        [Test]
+        public void DefaultStatementResolverProviderShouldReturnEmptyResolverList()
+        {
+            Assert.AreEqual(0, _statementFactory.GetResolvers().Count());
+        }
+    }
+}
