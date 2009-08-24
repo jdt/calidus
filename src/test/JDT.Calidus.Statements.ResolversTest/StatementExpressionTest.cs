07b374d51bb380a41368221c5f3a4dafb5ba177b
Jesper De Temmerman <jdt@users.noreply.github.com>
2009-08-24 23:17:33 +0200

    Added basic statement expression test

---
+++ src/test/JDT.Calidus.Statements.ResolversTest/StatementExpressionTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using JDT.Calidus.Statements.Resolvers;
+using JDT.Calidus.Tokens;
+
+namespace JDT.Calidus.Statements.ResolversTest
+{
+    [TestFixture]
+    public class StatementExpressionTest
+    {
+        private StatementExpression _expression;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _expression = new StatementExpression();
+        }
+
+        [Test]
+        public void EmptyExpressionShouldMatch()
+        {
+            IList<TokenBase> input = new List<TokenBase>();
+
+            Assert.True(_expression.Matches(input));
+        }
+    }
+}
