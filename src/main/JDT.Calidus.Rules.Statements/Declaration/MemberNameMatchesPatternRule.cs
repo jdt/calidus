21d9d9a9480c279a9eb471ac2fec6ffa33413105
Jesper De Temmerman <jesper@toron.be>
2009-08-30 12:24:48 +0200

    Implemented a rule to validate the pattern of a member declaration identifier

---
+++ src/main/JDT.Calidus.Rules.Statements/Declaration/MemberNameMatchesPatternRule.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using JDT.Calidus.Statements;
+using JDT.Calidus.Statements.Declaration;
+
+namespace JDT.Calidus.Rules.Statements.Declaration
+{
+    /// <summary>
+    /// This class implements a rule to check if the member name
+    /// of a member declaration matches a certain pattern
+    /// </summary>
+    public class MemberNameMatchesPatternRule 
+        : RuleBase
+    {
+        private Regex _expression;
+
+        /// <summary>
+        /// Creates a new instance of this class
+        /// </summary>
+        /// <param name="expression">The expression to match with</param>
+        public MemberNameMatchesPatternRule(Regex expression)
+        {
+            _expression = expression;
+        }
+
+        /// <summary>
+        /// Checks if the rule validates the supplied statement
+        /// </summary>
+        /// <param name="statement">The statement</param>
+        /// <returns>True if validates, otherwise false</returns>
+        public override bool Validates(StatementBase statement)
+        {
+            return statement is MemberStatement;
+        }
+
+        /// <summary>
+        /// Checks if the rule is valid for the statement
+        /// </summary>
+        /// <param name="statement">The statement</param>
+        /// <returns>True if valid, otherwise false</returns>
+        public override bool IsValidFor(StatementBase statement)
+        {
+            MemberStatement memberStatement = (MemberStatement) statement;
+            return _expression.IsMatch(memberStatement.MemberNameToken.Content);
+        }
+    }
+}
