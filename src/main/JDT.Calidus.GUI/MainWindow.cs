fe0bc4531ea8dc7f16b9aec30d296b0a2a7c574a
Jesper De Temmerman <jdt@users.noreply.github.com>
2009-09-03 21:45:50 +0200

    Added project and project support to basic UI

--- src/main/JDT.Calidus.GUI/MainWindow.cs
+++ src/main/JDT.Calidus.GUI/MainWindow.cs
@@ -7,20 +7,75 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-using JDT.Calidus.Projects.Files;
+using JDT.Calidus.Projects;
 
 namespace JDT.Calidus.GUI
 {
     public partial class MainWindow : Form
     {
+        private const String NOT_SET = "<Not Set>";
+        private const String TITLE = "Calidus GUI Test Runner - [{0}]";
+        private const String NEW_PROJECT = "New Project";
+        private const String UNSAVED_PROJECT = "Unsaved Project";
+
         public MainWindow()
         {
             InitializeComponent();
+
+            CurrentProject = null;
+
+            DisplayProjectDetails();
+        }
+
+        #region Events
+
+        private void lnkSourceDirectory_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            FolderBrowserDialog browseDirectory = new FolderBrowserDialog();
+            browseDirectory.ShowNewFolderButton = false;
+            browseDirectory.ShowDialog(this);
+
+            String selectedDir = browseDirectory.SelectedPath;
+            if(selectedDir.Equals(String.Empty) == false)
+            {
+                CurrentProject = new CalidusProject(selectedDir, null);
+                DisplayProjectDetails();
+            }
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Application.Exit();
         }
+
+        #endregion
+
+        #region Methods
+
+        private void DisplayProjectDetails()
+        {
+            if(CurrentProject == null)
+            {
+                Text = String.Format(TITLE, NEW_PROJECT);
+                lnkSourceDirectory.Text = NOT_SET;
+            }
+            else
+            {
+                String projectName = UNSAVED_PROJECT;
+                if (CurrentProject.ProjectLocation != null)
+                    projectName = CurrentProject.Name;
+
+                Text = String.Format(TITLE, projectName);
+                lnkSourceDirectory.Text = CurrentProject.SourceLocation;
+            }
+        }
+
+        #endregion
+
+        #region Properties
+
+        private CalidusProject CurrentProject { get; set; }
+
+        #endregion
     }
 }
