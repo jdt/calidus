9940f8db3bb6c2018ada5438f1abd727452a8a82
Jesper De Temmerman <jesper@toron.be>
2009-09-05 01:25:26 +0200

    Added basic statement rule factory

---
+++ src/main/JDT.Calidus.Rules.Statements/StatementRuleFactory.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JDT.Calidus.Common.Rules.Statements;
+using JDT.Calidus.Common.Statements;
+
+namespace JDT.Calidus.Rules.Statements
+{
+    /// <summary>
+    /// This class is the main calidus statement rule factory
+    /// </summary>
+    public class StatementRuleFactory : IStatementRuleFactory
+    {
+        /// <summary>
+        /// Gets the list of statement rules
+        /// </summary>
+        /// <returns>The rules</returns>
+        public IList<StatementRuleBase> GetStatementRules()
+        {
+            List<StatementRuleBase> result = new List<StatementRuleBase>();
+
+            return result;
+        }
+    }
+}
