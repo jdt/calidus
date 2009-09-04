0d4292c69e4eb5d90faf0531b2192bbedce1ca37
Jesper De Temmerman <jesper@toron.be>
2009-09-05 00:48:34 +0200

    Implemented rule name and category support

---
+++ src/main/JDT.Calidus.Common/Rules/RuleCategories.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JDT.Calidus.Common.Rules
+{
+    /// <summary>
+    /// This class provides a set of builtin rule categories
+    /// </summary>
+    public static class RuleCategories
+    {
+        /// <summary>
+        /// Get the naming category
+        /// </summary>
+        public static String Naming
+        {
+            get
+            {
+                return "Naming";
+            }
+        }
+    }
+}
