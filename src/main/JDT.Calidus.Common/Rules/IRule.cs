bf6f89898d101ac54ec11e0beb80668f452fcb3a
Jesper De Temmerman <jdt@users.noreply.github.com>
2009-09-05 00:35:06 +0200

    Added calidus rule provider and IRule

---
+++ src/main/JDT.Calidus.Common/Rules/IRule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JDT.Calidus.Common.Rules
+{
+    /// <summary>
+    /// This class is implemented by all rules
+    /// </summary>
+    public interface IRule
+    {
+    }
+}
