b834e9a09f86d9a06e9d68977f9149b5b706b16f
Jesper De Temmerman <jesper@toron.be>
2009-09-05 01:15:01 +0200

    Refactored creating instances from assemblies into a separate class

--- src/main/JDT.Calidus.Providers/StatementFactoryProviders/AssemblyBasedStatementFactoryProvider.cs
+++ src/main/JDT.Calidus.Providers/StatementFactoryProviders/AssemblyBasedStatementFactoryProvider.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using System.IO;
 using JDT.Calidus.Common.Providers;
 using JDT.Calidus.Common.Statements;
+using JDT.Calidus.Common.Util;
 
 namespace JDT.Calidus.Providers.StatementFactoryProviders
 {
@@ -14,42 +15,14 @@ namespace JDT.Calidus.Providers.StatementFactoryProviders
     /// </summary>
     public class AssemblyBasedStatementFactoryProvider : IStatementFactoryProvider
     {
-        private static IEnumerable<Assembly> GetCurrentDirectoryAssemblyList()
-        {
-            IList<Assembly> assemblyList = new List<Assembly>();
-            foreach(String anAssembly in Directory.GetFiles(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "*.dll"))
-            {
-                assemblyList.Add(Assembly.LoadFile(anAssembly));
-            }
-            return assemblyList;
-        }
-
-        private IEnumerable<Assembly> _assemblies;
+        private AssemblyBasedAssignableClassCreator _creator;
 
         /// <summary>
         /// Create a new instance of this class
         /// </summary>
         public AssemblyBasedStatementFactoryProvider()
-            : this(GetCurrentDirectoryAssemblyList())
         {
-        }
-
-        /// <summary>
-        /// Create a new instance of this class
-        /// </summary>
-        /// <param name="assemblyToParse">The assembly to parse</param>
-        public AssemblyBasedStatementFactoryProvider(Assembly assemblyToParse)
-            : this(new Assembly[] { assemblyToParse})
-        {
-        }
-
-        /// <summary>
-        /// Create a new instance of this class
-        /// </summary>
-        /// <param name="assembliesToParse"></param>
-        public AssemblyBasedStatementFactoryProvider(IEnumerable<Assembly> assembliesToParse)
-        {
-            _assemblies = assembliesToParse;
+            _creator = new AssemblyBasedAssignableClassCreator();
         }
 
         /// <summary>
@@ -58,26 +31,7 @@ namespace JDT.Calidus.Providers.StatementFactoryProviders
         /// <returns>The factories</returns>
         public IEnumerable<IStatementFactory> GetFactories()
         {
-            IList<IStatementFactory> factories = new List<IStatementFactory>();
-
-            //loads all types in the assembly and checks that they are
-            //implementations of the IStatementFactory
-            foreach (Assembly anAssembly in _assemblies)
-            {
-                foreach (Type aType in anAssembly.GetTypes())
-                {
-                    //make sure to ignore the interface itself
-                    if (typeof(IStatementFactory).IsAssignableFrom(aType)
-                        && aType.IsInterface == false
-                        && aType.IsAbstract == false
-                        )
-                    { 
-                        factories.Add((IStatementFactory)Activator.CreateInstance(aType));
-                    }
-                }
-            }
-
-            return factories;
+            return _creator.GetInstancesOf<IStatementFactory>();
         }
     }
 }
---
+++ src/test/unit/JDT.Calidus.CommonTest/Util/AssemblyBasedAssignableClassCreatorTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JDT.Calidus.Common.Util;
+using NUnit.Framework;
+
+namespace JDT.Calidus.CommonTest.Util
+{
+    public interface IAssemblyBasedAssignableClassCreatorTest
+    {
+    }
+
+    public class AssemblyBasedAssignableClassCreatorTestInstance : IAssemblyBasedAssignableClassCreatorTest
+    {
+        // override object.Equals
+        public override bool Equals(object obj)
+        {
+            //       
+            // See the full list of guidelines at
+            //   http://go.microsoft.com/fwlink/?LinkID=85237  
+            // and also the guidance for operator== at
+            //   http://go.microsoft.com/fwlink/?LinkId=85238
+            //
+
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        // override object.GetHashCode
+        public override int GetHashCode()
+        {
+            return 3;
+        }
+    }
+
+    [TestFixture]
+    public class AssemblyBasedAssignableClassCreatorTest
+    {
+        private AssemblyBasedAssignableClassCreator _creator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _creator = new AssemblyBasedAssignableClassCreator(GetType().Assembly);
+        }
+
+        [Test]
+        public void AssemblyBasedAssignableClassCreatorTestShouldReturnClassesInAssemblyList()
+        {
+            IList<IAssemblyBasedAssignableClassCreatorTest> expected = new List<IAssemblyBasedAssignableClassCreatorTest>();
+            expected.Add(new AssemblyBasedAssignableClassCreatorTestInstance());
+
+            CollectionAssert.AreEquivalent(expected, _creator.GetInstancesOf<IAssemblyBasedAssignableClassCreatorTest>());
+        }
+
+        [Test]
+        public void AssemblyBasedAssignableClassCreatorTestShouldNotReturnAbstractClassesInAssemblyList()
+        {
+            IList<IAssemblyBasedAssignableClassCreatorTest> expected = new List<IAssemblyBasedAssignableClassCreatorTest>();
+            expected.Add(new AssemblyBasedAssignableClassCreatorTestInstance());
+
+            CollectionAssert.AreEquivalent(expected, _creator.GetInstancesOf<IAssemblyBasedAssignableClassCreatorTest>());
+        }
+    }
+}
