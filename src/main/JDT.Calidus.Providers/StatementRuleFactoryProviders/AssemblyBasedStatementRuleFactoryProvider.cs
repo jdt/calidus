91ae53fae48f8e5c048d13396748284b325ea436
Jesper De Temmerman <jesper@toron.be>
2009-09-05 01:17:39 +0200

    Added assembly based statement rule factory provider

---
+++ src/main/JDT.Calidus.Providers/StatementRuleFactoryProviders/AssemblyBasedStatementRuleFactoryProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JDT.Calidus.Common.Providers;
+using JDT.Calidus.Common.Statements;
+using JDT.Calidus.Common.Util;
+
+namespace JDT.Calidus.Providers.StatementRuleFactoryProviders
+{
+    /// <summary>
+    /// Assembly-based implementation of the IStatementRuleFactoryProvider
+    /// </summary>
+    public class AssemblyBasedStatementRuleFactoryProvider : IStatementRuleFactoryProvider
+    {
+        private AssemblyBasedAssignableClassCreator _creator;
+
+        /// <summary>
+        /// Create a new instance of this class
+        /// </summary>
+        public AssemblyBasedStatementRuleFactoryProvider()
+        {
+            _creator = new AssemblyBasedAssignableClassCreator();
+        }
+
+        /// <summary>
+        /// Loads the statement rule factories from the provider
+        /// </summary>
+        /// <returns>The factory</returns>
+        public IEnumerable<IStatementRuleFactory> GetStatementRuleFactories()
+        {
+            return _creator.GetInstancesOf<IStatementRuleFactory>();
+        }
+    }
+}
