56614aaca0ed3851fe70b8361d58ee579ce11438
Jesper De Temmerman <jesper@toron.be>
2009-09-25 10:15:12 +0200

    Added write capabilities to the file configuration

--- src/main/JDT.Calidus.Common/Rules/Configuration/Factories/FileRuleConfigurationFactory.cs
+++ src/main/JDT.Calidus.Common/Rules/Configuration/Factories/FileRuleConfigurationFactory.cs
@@ -14,48 +14,19 @@ namespace JDT.Calidus.Common.Rules.Configuration.Factories
     /// </summary>
     public class FileRuleConfigurationFactory : IRuleConfigurationFactory
     {
+        private IFileRuleConfigurationFactoryStreamProvider _provider;
+        private XDocument _doc;
+
         private IList<IRuleConfiguration> _configList;
 
         /// <summary>
-        /// Create a new instance of this class
+        /// Createa a new instance of this class
         /// </summary>
-        /// <param name="file">The file to parse</param>
-        public FileRuleConfigurationFactory(String file)
-            : this(new XmlTextReader(Path.GetFullPath(file)))
+        /// <param name="provider">The provider to use</param>
+        public FileRuleConfigurationFactory(IFileRuleConfigurationFactoryStreamProvider provider)
         {
-        }
-
-        /// <summary>
-        /// Create a new instance of this class
-        /// </summary>
-        /// <param name="reader">The reader to use</param>
-        public FileRuleConfigurationFactory(XmlReader reader)
-        {
-            _configList = new List<IRuleConfiguration>();
-
-            XDocument doc = XDocument.Load(reader);
-
-            //parse all rule declarations
-            var result = from e in doc.Root.Elements("rule")
-                         select new DefaultRuleConfiguration
-                         {
-                             Description = e.Element("description").Value,
-                             Rule = Type.GetType(e.Attribute("type").Value, true),
-                             Parameters = (
-                                              from f in e.Elements("params")
-                                              select new
-                                              {
-                                                  Name = f.Element("param").Attribute("name").Value,
-                                                  Value = f.Element("param").Value
-                                              }
-                                          ).ToDictionary(p => p.Name, p => p.Value)
-
-                         };
-
-            foreach (DefaultRuleConfiguration w in result)
-            {
-                _configList.Add(w);
-            }
+            _provider = provider;
+            LoadDocumentContent();
         }
 
         /// <summary>
@@ -67,5 +38,85 @@ namespace JDT.Calidus.Common.Rules.Configuration.Factories
         {
             return _configList.FirstOrDefault(p => p.Rule.Equals(type));
         }
+
+        /// <summary>
+        /// Sets the configuration of the specified rule type
+        /// </summary>
+        /// <param name="ruleType">The rule type</param>
+        /// <param name="description">The description</param>
+        /// <param name="parameters">The parameters</param>
+        public void Set(Type ruleType, String description, IDictionary<String, String> parameters)
+        {
+            _configList.Add(new DefaultRuleConfiguration(ruleType, description, parameters));
+            WriteDocumentContent();
+        }
+
+        #region Write and load
+
+            private void LoadDocumentContent()
+            {
+                _configList = new List<IRuleConfiguration>();
+                _doc = XDocument.Load(_provider.GetReader());
+                //parse all rule declarations
+                var result = from e in _doc.Root.Elements("rule")
+                             select new DefaultRuleConfiguration
+                             {
+                                 Description = e.Element("description").Value,
+                                 Rule = Type.GetType(e.Attribute("type").Value, true),
+                                 Parameters = (
+                                                  from f in e.Elements("params")
+                                                  select new
+                                                  {
+                                                      Name = f.Element("param").Attribute("name").Value,
+                                                      Value = f.Element("param").Value
+                                                  }
+                                              ).ToDictionary(p => p.Name, p => p.Value)
+
+                             };
+
+                foreach (DefaultRuleConfiguration w in result)
+                {
+                    _configList.Add(w);
+                }
+            }
+
+            private void WriteDocumentContent()
+            {
+                //clear all rules
+                //remember to use ToList, see http://msdn.microsoft.com/en-us/library/bb387088.aspx
+                foreach (XElement aRule in _doc.Root.Elements().ToList())
+                    aRule.Remove();
+
+                //and rewrite
+                foreach (IRuleConfiguration aConfiguration in _configList)
+                {
+                    String fullAssembly = aConfiguration.Rule.Assembly.FullName;
+                    String assembly = fullAssembly.Substring(0, fullAssembly.IndexOf(" ") - 1);
+
+                    XElement ruleElement = new XElement("rule", new XAttribute("type", aConfiguration.Rule.FullName + ", " + assembly));
+                    XElement descriptionElement = new XElement("description", aConfiguration.Description);
+
+                    //write parameters
+                    XElement parametersElement = new XElement("params");
+                    foreach (String key in aConfiguration.Parameters.Keys)
+                    {
+                        XElement paramElement = new XElement("param", aConfiguration.Parameters[key]);
+                        paramElement.Add(new XAttribute("name", key));
+
+                        parametersElement.Add(paramElement);
+                    }
+
+                    ruleElement.Add(descriptionElement);
+                    ruleElement.Add(parametersElement);
+
+                    _doc.Root.Add(ruleElement);
+                }
+
+                XmlWriter writer = _provider.GetWriter();
+                _doc.Save(writer);
+                writer.Flush();
+            }
+
+        #endregion
     }
 }
--- src/test/unit/JDT.Calidus.CommonTest/Rules/Configuration/Factories/FileRuleConfigurationFactoryTest.cs
+++ src/test/unit/JDT.Calidus.CommonTest/Rules/Configuration/Factories/FileRuleConfigurationFactoryTest.cs
@@ -10,9 +10,36 @@ using NUnit.Framework;
 
 namespace JDT.Calidus.CommonTest.Rules.Configuration.Factories
 {
+    public class TestStreamProvider : IFileRuleConfigurationFactoryStreamProvider
+    {
+        private XmlReader _reader;
+        private XmlWriter _writer;
+
+        public TestStreamProvider(XmlReader reader, XmlWriter writer)
+        {
+            _reader = reader;
+            _writer = writer;
+        }
+
+        public XmlReader GetReader()
+        {
+            return _reader;
+        }
+
+        public XmlWriter GetWriter()
+        {
+            return _writer;
+        }
+    }
+
     [TestFixture]
     public class FileRuleConfigurationFactoryTest
     {
+        private XmlWriter GetEmptyWriter()
+        {
+            return new XmlTextWriter(new MemoryStream(), Encoding.Default);
+        }
+
         [Test]
         public void ParsingStreamWithRegistrationShouldReturnInformation()
         {
@@ -34,7 +61,7 @@ namespace JDT.Calidus.CommonTest.Rules.Configuration.Factories
             Stream stream = new MemoryStream(Encoding.Default.GetBytes(bldr.ToString()));
             XmlReader reader = new XmlTextReader(stream);
 
-            FileRuleConfigurationFactory builder = new FileRuleConfigurationFactory(reader);
+            FileRuleConfigurationFactory builder = new FileRuleConfigurationFactory(new TestStreamProvider(reader, GetEmptyWriter()));
             IRuleConfiguration actual = builder.Get(typeof(UnCreatableRule));
 
             IDictionary<String, String> expectedParam = new Dictionary<String, String>();
@@ -66,7 +93,7 @@ namespace JDT.Calidus.CommonTest.Rules.Configuration.Factories
             Stream stream = new MemoryStream(Encoding.Default.GetBytes(bldr.ToString()));
             XmlReader reader = new XmlTextReader(stream);
 
-            FileRuleConfigurationFactory builder = new FileRuleConfigurationFactory(reader);
+            FileRuleConfigurationFactory builder = new FileRuleConfigurationFactory(new TestStreamProvider(reader, GetEmptyWriter()));
             IRuleConfiguration actual = builder.Get(typeof(UnCreatableRule));
 
             IDictionary<String, String> expectedParam = new Dictionary<String, String>();
@@ -101,9 +128,52 @@ namespace JDT.Calidus.CommonTest.Rules.Configuration.Factories
             Assert.Throws<TypeLoadException>(
                 delegate
                 {
-                    FileRuleConfigurationFactory builder = new FileRuleConfigurationFactory(reader);
+                    FileRuleConfigurationFactory builder = new FileRuleConfigurationFactory(new TestStreamProvider(reader, GetEmptyWriter()));
                 }
                 );
+        }
+
+        [Test]
+        public void AddingRuleTypeShouldWriteDocumentToXmlWriter()
+        {
+            StringBuilder empty = new StringBuilder();
+            empty.Append(@"<?xml version=""1.0"" encoding=""utf-8""?>");
+            empty.Append("<rules>");
+            empty.Append("</rules>");
+
+            StringBuilder bldr = new StringBuilder();
+            bldr.Append(@"<?xml version=""1.0"" encoding=""utf-8""?>");
+            bldr.Append("<rules>");
+            bldr.Append(@"<rule type=""JDT.Calidus.CommonTest.Rules.UnCreatableRule, JDT.Calidus.CommonTest"">");
+            bldr.Append("<description>");
+            bldr.Append("Description text");
+            bldr.Append("</description>");
+            bldr.Append("<params>");
+            bldr.Append(@"<param name=""param1"">");
+            bldr.Append("theValue");
+            bldr.Append(@"</param>");
+            bldr.Append("</params>");
+            bldr.Append("</rule>");
+            bldr.Append("</rules>");
+
+            Stream writerStream = new MemoryStream();
+
+            XmlReader reader = new XmlTextReader(new StringReader(empty.ToString()));
+            XmlWriter writer = XmlTextWriter.Create(writerStream, new XmlWriterSettings());
+
+            Type type = Type.GetType("JDT.Calidus.CommonTest.Rules.UnCreatableRule, JDT.Calidus.CommonTest");
+            String desc = "Description text";
+            IDictionary<String, String> param = new Dictionary<String, String>();
+            param.Add("param1", "theValue");
+
+            FileRuleConfigurationFactory builder = new FileRuleConfigurationFactory(new TestStreamProvider(reader, writer));
+            builder.Set(type, desc, param);
+
+            writerStream.Seek(0, SeekOrigin.Begin);
+            TextReader actualReader = new StreamReader(writerStream);
+            String actual = actualReader.ReadToEnd();
+
+            Assert.AreEqual(bldr.ToString(), actual);
         }
     }
 }
