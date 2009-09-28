853b6258f9d9f624db330829cd389d6e9e178b67
Jesper De Temmerman <jesper@toron.be>
2009-09-28 16:39:37 +0200

    Added parameter types

---
+++ src/main/JDT.Calidus.Common/Rules/Configuration/IRuleConfigurationParameter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JDT.Calidus.Common.Rules.Configuration
+{
+    /// <summary>
+    /// This interface is implemented by rule configuration parameters
+    /// </summary>
+    public interface IRuleConfigurationParameter
+    {
+        /// <summary>
+        /// Gets the parameter type
+        /// </summary>
+        ParameterType ParameterType { get; }
+        /// <summary>
+        /// Gets the parameter name
+        /// </summary>
+        String Name { get; }
+        /// <summary>
+        /// Gets or sets the parameter value
+        /// </summary>
+        String Value { get; set; }
+    }
+}
