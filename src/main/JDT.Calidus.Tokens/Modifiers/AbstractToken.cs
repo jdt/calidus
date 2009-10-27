64f7ce859423a8ccfd19fb1b2828e7f52bbbe9b5
Jesper De Temmerman <jesper@toron.be>
2009-10-27 14:24:51 +0100

    Added abstract and static token support

---
+++ src/main/JDT.Calidus.Tokens/Modifiers/AbstractToken.cs
@@ -0,0 +1,43 @@
+﻿#region License
+    // <copyright>
+    //   Copyright 2009 Jesper De Temmerman 
+    //   Licensed under the Apache License, Version 2.0 (the "License"); 
+    //   you may not use this file except in compliance with the License. 
+    //   You may obtain a copy of the License at
+    //
+    //   http://www.apache.org/licenses/LICENSE-2.0 
+    //
+    //   Unless required by applicable law or agreed to in writing, software 
+    //   distributed under the License is distributed on an "AS IS" BASIS, 
+    //   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+    //   See the License for the specific language governing permissions and 
+    //   limitations under the License. 
+    // </copyright>
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JDT.Calidus.Common.Tokens;
+
+namespace JDT.Calidus.Tokens.Modifiers
+{
+    /// <summary>
+    /// This class represents an abstract token
+    /// </summary>
+    public class AbstractToken : TokenBase
+    {
+        /// <summary>
+        /// Create a new instance of this class
+        /// </summary>
+        /// <param name="line">The line</param>
+        /// <param name="col">The column</param>
+        /// <param name="pos">The position</param>
+        /// <param name="content">The content</param>
+        public AbstractToken(int line, int col, int pos, String content)
+            : base(line, col, pos, content)
+        {
+        }
+    }
+}
