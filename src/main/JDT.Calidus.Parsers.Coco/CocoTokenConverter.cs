afe0be1350157f1430726801573a7c8fc796c900
Jesper De Temmerman <jdt@users.noreply.github.com>
2009-10-28 17:24:14 +0100

    Added dot token parsing

--- src/main/JDT.Calidus.Parsers.Coco/CocoTokenConverter.cs
+++ src/main/JDT.Calidus.Parsers.Coco/CocoTokenConverter.cs
@@ -84,6 +84,7 @@ namespace JDT.Calidus.Parsers.Coco
             _contentTokenTypeMap.Add(Parser._class, typeof(ClassToken));
             _contentTokenTypeMap.Add(Parser._usingKW, typeof(UsingToken));
             _contentTokenTypeMap.Add(Parser._namespace, typeof(NameSpaceToken));
+            _contentTokenTypeMap.Add(Parser._dot, typeof(DotToken));
         }
 
         /// <summary>
