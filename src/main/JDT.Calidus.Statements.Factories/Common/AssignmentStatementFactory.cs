c48c1edc37d3216eca0cad54baf003e583cfcb23
Jesper De Temmerman <jesper@toron.be>
2009-11-01 23:33:04 +0100

    Added support for assignment statement parsing

---
+++ src/main/JDT.Calidus.Statements.Factories/Common/AssignmentStatementFactory.cs
@@ -0,0 +1,56 @@
+﻿#region License
+    // <copyright>
+    //   Copyright 2009 Jesper De Temmerman 
+    //   Licensed under the Apache License, Version 2.0 (the "License"); 
+    //   you may not use this file except in compliance with the License. 
+    //   You may obtain a copy of the License at
+    //
+    //   http://www.apache.org/licenses/LICENSE-2.0 
+    //
+    //   Unless required by applicable law or agreed to in writing, software 
+    //   distributed under the License is distributed on an "AS IS" BASIS, 
+    //   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+    //   See the License for the specific language governing permissions and 
+    //   limitations under the License. 
+    // </copyright>
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using JDT.Calidus.Common.Statements;
+using JDT.Calidus.Common.Tokens;
+using JDT.Calidus.Statements.Common;
+using JDT.Calidus.Statements.Factories.Fluent;
+using JDT.Calidus.Tokens.Common;
+
+namespace JDT.Calidus.Statements.Factories.Common
+{
+    /// <summary>
+    /// This class creates assignment statements
+    /// </summary>
+    public class AssignmentStatementFactory : FluentStatementFactory<AssignmentStatement>
+    {
+        protected override AssignmentStatement BuildStatement(IEnumerable<TokenBase> input)
+        {
+            return new AssignmentStatement(input);
+        }
+
+        protected override bool IsValidContext(IStatementContext context)
+        {
+            return true;
+        }
+
+        protected override IStatementExpression Expression
+        {
+            get
+            {
+                StatementExpression expression = new StatementExpression();
+                expression.StartsWith<AssignmentToken>();
+
+                return expression;
+            }
+        }
+    }
+}
